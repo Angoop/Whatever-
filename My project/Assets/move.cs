f55c11af0aadfbb1231cebc3074969dcc1f569ec
AngiePie <30pecang@efcps.net>
2024-05-04 15:21:32 -0700

    gahaha

--- My project/Assets/move.cs
+++ My project/Assets/move.cs
@@ -4,25 +4,45 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    public float speed;
-    public Vector3 jumpForce = new Vector3( 0.0f, 2.0f, 0.0f );
+    public float speed = 5f;
+    public Vector3 jumpForce = new Vector3( 0.0f, 10.0f, 0.0f );
     private Rigidbody rb;
+    public float rotationSpeed = 1f;
+    private float yRotate = 0.0f;
+
+    private Vector3 startPosition = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        startPosition = transform.position;
     }
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)&& isGrounded())
         {
             rb.AddForce(jumpForce, ForceMode.Impulse);
         }
     }
+    void RotateView()
+    {
+        yRotate += Input.GetAxis("Mouse X") * rotationSpeed;
+
+        transform.eulerAngles = new Vector3(0, yRotate, 0);
+    }
+
+    bool isGrounded()
+    {
+        int layerMask = LayerMask.GetMask("Ground");
+        Vector3 offset = new Vector3(0.0f, -0.5f, 0.0f);
+        return Physics.Raycast(transform.position + offset,
+          Vector3.down, 0.6f, layerMask);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveDir = Vector2.zero;
+        Vector3 moveDir = Vector3.zero;
         if (Input.GetKey(KeyCode.A))
         {
             moveDir.x = -1.0f;
@@ -40,14 +60,17 @@ public class move : MonoBehaviour
             moveDir.x = 1.0f;
         }
         Vector3 movement = transform.forward * moveDir.y + transform.right * moveDir.x;
-        moveDir = moveDir * speed * Time.deltaTime;
+        moveDir = movement * speed * Time.deltaTime;
 
         transform.Translate(moveDir);
 
         Jump();
+        RotateView();
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        transform.position = startPosition;
+        rb.velocity = Vector3.zero;
     }
 }
-
-/*Vector3 position = transform.position;
-      position.x = position.x + speed * Time.deltaTime;
-      transform.position = position;*/
