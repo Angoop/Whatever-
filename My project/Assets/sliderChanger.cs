4fa586573edef1c38a1b25642651906ae7bb3e84
Angie P. <derpycupcakep@gmail.com>
2024-06-08 17:01:08 -0700

    wertyu

---
+++ My project/Assets/sliderChanger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sliderChanger : MonoBehaviour
+{
+    public Slider changer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        changer = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    public void ChangeValue (float increment)
+    {
+        changer.value += increment;
+    }
+}
